5098200f04fa1fd1b2906ad9a1253f9304a7ff8c
Your Name <you@example.com>
2018-03-23 17:39:10 -0400

    add find_dog to the repo

---
+++ find_dog.py
@@ -0,0 +1,83 @@
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import requests
+import json
+
+'''
+README:
+This script run with Python 2.7.12.
+To run this script you need to install requests and json module"
+To send the email with gmail, you need to turn on the setting for allow low security app to access
+'''
+
+def sendEmail(dogname):
+
+    '''
+    (str)->NoneType
+    Send an email to inform myself that the dog that I am waiting for is now
+    available on the website
+    '''
+
+    sender = '****@*****.com'
+    receivers = ['****@*****.com', 'aaaa@****.com']
+    password = '*********'
+    subject = 'Found DOG!'
+    
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (sender, ", ".join(receivers), subject, (dogname + ' is available!!!'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(sender,password)  
+        server.sendmail(sender, receivers, message)
+        server.close()
+        print "Email sent successfully !"
+    except smtplib.SMTPException, e:
+        print "Error: can't send it, below is the Exception: " + str(e)
+        
+
+def requestPage(url):
+
+    '''
+    Make request to endpoint to get repoonse
+    returns a list of dictionaries of dog information
+    '''
+
+    response = requests.get(url)
+    data = response.json()
+    return data.get('AdoptableSearchResult').get('XmlNode')
+
+
+def findDog(dogs, keys):
+
+    '''
+    check if any of the dog's name in keys shows up on the website
+    '''
+
+    for dog in dogs:
+        if (dog is not None) and (getName(dog) in keys):
+            print "found it"
+            sendEmail(getName(dog))
+
+
+def getName(dog):
+
+    '''
+    get a dog's name
+    '''
+
+    return dog.get('adoptableSearch').get('Name')
+    
+
+if __name__ == "__main__":
+
+    while True:
+        dogs = requestPage("https://www.torontohumanesociety.com/api/api.php?action=getAnimalsForSpeciesId&id=1&stageId=2")
+        print(dogs)
+        findDog(dogs, ['Alfreda', 'Ashley', 'Twinkle'])
+        time.sleep(300)
+
